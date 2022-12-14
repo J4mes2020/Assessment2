5d41d8e39953451a5bbeb548c910755e33b1c9ae
Joey <james.lawrence@live.co.uk>
2022-12-14 14:40:36 +0000

    Inital

--- src/main/java/dev/joeyfoxo/Application.java
+++ src/main/java/dev/joeyfoxo/Application.java
@@ -10,10 +10,10 @@ import java.math.RoundingMode;
 
 public class Application {
     public static void main(String[] args) {
-        LoanAccount johnsLoanAccount = new LoanAccount(22000);
-        CurrentAccount johnsCurrentAccount = new CurrentAccount();
-        LoanAccount maryLoanAccount = new LoanAccount(18000);
-        SavingsAccount marySavingsAccount = new SavingsAccount(200);
+        LoanAccount johnsLoanAccount = new LoanAccount(22000); //Instantiating johns loan account with 22000
+        CurrentAccount johnsCurrentAccount = new CurrentAccount(); //Instantiating johns current account
+        LoanAccount maryLoanAccount = new LoanAccount(18000); //Instantiating mary's  loan account with 18000
+        SavingsAccount marySavingsAccount = new SavingsAccount(200);//Instantiating marys savings account
 
         payDebt(johnsLoanAccount, johnsCurrentAccount, 1300, 500, "John");
         payDebt(maryLoanAccount, marySavingsAccount, 1400, 350, "Mary");
@@ -21,22 +21,21 @@ public class Application {
     }
 
     private static <T extends Account> void payDebt(LoanAccount loanAccount, T account, int depositAmmount, int paymentAmount, String name) {
-        if (!(account instanceof LoanAccount)) {
-            int counter = 0;
-            while (loanAccount.getBalance() > 0) {
-                account.makeDeposit(depositAmmount);
-                loanAccount.makePayment(account, paymentAmount);
-                account.addInterest();
-                loanAccount.addInterest();
-                counter++;
+        if (!(account instanceof LoanAccount)) { //If the provided account isn't a loan account
+            int monthCounter = 0;
+            while (loanAccount.getBalance() > 0) { //While the debt hasn't been paid off
+                account.makeDeposit(depositAmmount); //Add the deposit amount to mary or johns account
+                loanAccount.makePayment(account, paymentAmount); //Make a transfer and pay off a set amount of debt
+                account.addInterest(); //Add intrest to their account
+                loanAccount.addInterest(); //Add intrest to the loan account
+                monthCounter++; //Increase the month by one
             }
 
-            account.makeDeposit(Math.abs(loanAccount.getBalance()));
-            loanAccount.setBalance(0);
-            System.out.println("It'll take " + name + " " + counter + " months to repay their loan");
+            account.makeDeposit(Math.abs(loanAccount.getBalance())); //If the balance is below 0 add the extra back to their account
+            loanAccount.setBalance(0); //And a loan cannot be negative so set it to 0
+            System.out.println("It'll take " + name + " " + monthCounter + " months to repay their loan");
             System.out.println("and they have £" + round(account.getBalance(), 2) + " in their account\n");
-        }
-        else {
+        } else {
             System.out.println("Sorry you cannot pay debt out of a loan account");
         }
     }
